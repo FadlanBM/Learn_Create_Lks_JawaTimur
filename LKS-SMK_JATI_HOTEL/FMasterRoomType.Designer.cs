2929b9c65665513f5bc996146d22fbd49fe3b9ee
FadlanBM <104897512+FadlanBM@users.noreply.github.com>
2023-09-12 21:03:20 +0700

    addDatabse

---
+++ LKS-SMK_JATI_HOTEL/FMasterRoomType.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace LKS_SMK_JATI_HOTEL
+{
+    partial class FMasterRoomType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_priceRoom = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.up_capacity = new System.Windows.Forms.NumericUpDown();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.up_capacity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Capacity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Room Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Photo";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(104, 26);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(330, 20);
+            this.tb_name.TabIndex = 5;
+            // 
+            // tb_priceRoom
+            // 
+            this.tb_priceRoom.Location = new System.Drawing.Point(104, 100);
+            this.tb_priceRoom.Name = "tb_priceRoom";
+            this.tb_priceRoom.Size = new System.Drawing.Size(330, 20);
+            this.tb_priceRoom.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(109, 141);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(325, 180);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column4,
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(21, 327);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(413, 181);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(13, 510);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(421, 38);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(339, 10);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 24);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Cancel";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(258, 9);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 24);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(176, 9);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(94, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // up_capacity
+            // 
+            this.up_capacity.Location = new System.Drawing.Point(104, 63);
+            this.up_capacity.Name = "up_capacity";
+            this.up_capacity.Size = new System.Drawing.Size(330, 20);
+            this.up_capacity.TabIndex = 11;
+            this.up_capacity.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "id";
+            this.Column4.Name = "Column4";
+            this.Column4.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Capacity";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Room Price";
+            this.Column3.Name = "Column3";
+            // 
+            // FMasterRoomType
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 555);
+            this.Controls.Add(this.up_capacity);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tb_priceRoom);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FMasterRoomType";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FMainMasterRoomType";
+            this.Load += new System.EventHandler(this.FMasterRoomType_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.up_capacity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_priceRoom;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown up_capacity;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
---
+++ LKS-SMK_JATI_HOTEL/FMasterRoomType.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LKS_SMK_JATI_HOTEL
+{   
+    public partial class FMasterRoomType : Form
+    {
+        OpenFileDialog ofd;
+        private string path=Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName+@"\image\";
+        AppDbContextDataContext dbcontext;
+        private string id = string.Empty;
+        public FMasterRoomType()
+        {
+            dbcontext=new AppDbContextDataContext();
+            ofd= new OpenFileDialog();  
+            InitializeComponent();  
+        }
+
+        private void FMasterRoomType_Load(object sender, EventArgs e)
+        {
+            normalMode();
+            getData();           
+        }
+
+        private void getData() {
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows=false;
+            dataGridView1.Rows.Clear(); 
+            var data = (from ry in dbcontext.RoomTypes
+                        select new
+                        {
+                            id=ry.ID,
+                            name=ry.Name,
+                            capacity=ry.Capacity,
+                            roomprice=ry.RoomPrice
+                        }).ToList();
+            foreach (var item in data)
+            {
+                var num = dataGridView1.Rows.Add();
+                dataGridView1.Rows[num].Cells[0].Value = item.id;
+                dataGridView1.Rows[num].Cells[1].Value = item.name;
+                dataGridView1.Rows[num].Cells[2].Value = item.capacity;
+                dataGridView1.Rows[num].Cells[3].Value = string.Format("{0:n}",item.roomprice);
+               
+            }
+        }
+
+        private void loadData() { 
+        
+            var data=dbcontext.RoomTypes.Where(r=>r.ID==int.Parse(id)).FirstOrDefault();
+            tb_name.Text = data.Name;
+            tb_priceRoom.Text = data.RoomPrice.ToString();
+            up_capacity.Value=data.ca;
+            var nameImage = path + data.Photo;
+            if (!File.Exists(nameImage))
+            {
+                return;
+            }
+            using (var image=File.OpenRead(nameImage))
+            {
+                pictureBox1.Image = new Bitmap(image);
+            }
+        }
+
+        private void normalMode() { 
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled=true;
+            button3.Enabled= true;
+            button4.Enabled= true;
+            button5.Enabled= false;
+            button6.Enabled= false;
+            tb_name.Enabled= false;
+            tb_priceRoom.Enabled= false;
+            up_capacity.Enabled= false;
+        }
+        private void insertMode() {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            tb_name.Enabled = true;
+            tb_priceRoom.Enabled = true;
+            up_capacity.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            insertMode();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Apakah anda yakin ingin membatalkan aksi","Warning",MessageBoxButtons.YesNo,MessageBoxIcon.Warning);
+            if (DialogResult.Yes==dialog)
+            {
+                normalMode();
+                clearForm();
+            }                        
+        }
+
+        private void clearForm() {
+            tb_name.Text = "";
+            tb_priceRoom.Text = "";
+            up_capacity.Value = 0;
+            pictureBox1.Image = null;
+        }
+        private void updateMode() {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = true;            
+            button4.Enabled = false;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            tb_name.Enabled = true;
+            tb_priceRoom.Enabled = true;
+            up_capacity.Enabled = true;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            updateMode();
+        }
+
+        private void deleteMode() {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            tb_name.Enabled = true;
+            tb_priceRoom.Enabled = true;
+            up_capacity.Enabled = true;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            deleteMode();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "(Files | *jpg;*png;*jpeg)";
+            ofd.Multiselect=false;
+            if (DialogResult.OK==ofd.ShowDialog())
+            {
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var lanjut = true;
+            if (tb_name.Text.Length == 0)
+            {
+               lanjut=false;
+                errorProvider1.SetError(tb_name, "Form nama belum terisi");
+            }      
+            else
+            {
+                errorProvider1.SetError(tb_name,"");
+            }
+
+
+            if (up_capacity.Value == 0)
+            {
+                lanjut = false;
+                errorProvider1.SetError(up_capacity, "Form capacity belum terisi");
+            }
+            else
+            {
+                errorProvider1.SetError(up_capacity, "");
+            }
+
+
+            if (tb_priceRoom.Text.Length == 0)
+            {
+                lanjut = false;
+                errorProvider1.SetError(tb_priceRoom, "Form Pric eRoom belum terisi");
+            }
+            else
+            {
+                errorProvider1.SetError(tb_priceRoom, "");
+            }
+
+            if (pictureBox1.Image == null)
+            {
+                lanjut = false;
+                errorProvider1.SetError(pictureBox1, "Picture belum di masukkan");
+            }
+            else
+            {
+                errorProvider1.SetError(pictureBox1, "");
+            }
+
+            if (!lanjut)
+            {
+                return;
+            }         
+         
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            if (button2.Enabled==true)
+            {
+                var nameImage = DateTime.Now.Ticks.ToString() + Path.GetFileName(ofd.FileName);
+               RoomType roomType = new RoomType();
+                roomType.Name=tb_name.Text;
+                roomType.Capacity =(int)up_capacity.Value;
+                roomType.RoomPrice = int.Parse( tb_priceRoom.Text.Replace(",",""));
+                roomType.Photo = nameImage;
+                File.Copy(ofd.FileName, path + nameImage);
+                dbcontext.RoomTypes.InsertOnSubmit(roomType);
+                dbcontext.SubmitChanges();
+                MessageBox.Show("Berhasil input data Room Type","Information",MessageBoxButtons.OK, MessageBoxIcon.Information);
+                getData();
+                clearForm();
+                normalMode();
+            }
+            if (button3.Enabled==true)
+            {
+                MessageBox.Show("Udapte");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (button3.Enabled == true) {
+                id = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                loadData();
+            }
+           
+        }
+    }
+}
